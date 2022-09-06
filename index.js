ab914364630070be6d49f604baeed7fa244112f0
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-09-06 16:37:46 -0600

    created intern, engineer, and manager questions in separate arrays

---
+++ index.js
@@ -0,0 +1,52 @@
+///Manager prompts
+const managerQuestions = [{
+    type: "input",
+    message: "What is the manager's name?",
+    name: "managerName",
+}, {
+    type: "input",
+    message: "What is the manager's id?",
+    name: "managerId",
+}, {
+    type: "input",
+    message: "What is the manager's email?",
+    name: "managerEmail",
+}, {
+    type: "input",
+    message: "What is the manager's officenumber?",
+    name: "officeNumber",
+}]
+const engineerQuestions = [{
+    type: "input",
+    message: "What is the engineer's name?",
+    name: "engineerName",
+}, {
+    type: "input",
+    message: "What is the engineer's id?",
+    name: "engineerId",
+}, {
+    type: "input",
+    message: "What is the engineer's email?",
+    name: "engineerEmail",
+}, {
+    type: "input",
+    message: "What is the engineer's github?",
+    name: "github",
+}]
+const internQuestions = [{
+    type: "input",
+    message: "What is the intern's name?",
+    name: "internName",
+}, {
+    type: "input",
+    message: "What is the intern's id?",
+    name: "internId",
+}, {
+    type: "input",
+    message: "What is the intern's email?",
+    name: "internEmail",
+}, {
+    type: "input",
+    message: "What school does the intern attend?",
+    name: "school",
+}]
