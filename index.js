e528ef89b20e9f37ba89df5bacd5eace9bf7245c
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-09-07 13:48:09 -0600

    created function to prompt the user using inquirer and created file that would hold function to create the html

--- index.js
+++ index.js
@@ -1,52 +1,142 @@
+const createHTML = require('./src/createHTML.js')
+
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// node modules 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+const teamArr = [];
+
 ///Manager prompts
 const managerQuestions = [{
     type: "input",
     message: "What is the manager's name?",
-    name: "managerName",
+    name: "name",
 }, {
     type: "input",
     message: "What is the manager's id?",
-    name: "managerId",
+    name: "id",
 }, {
     type: "input",
     message: "What is the manager's email?",
-    name: "managerEmail",
+    name: "email",
 }, {
     type: "input",
     message: "What is the manager's officenumber?",
     name: "officeNumber",
 }]
-const engineerQuestions = [{
-    type: "input",
-    message: "What is the engineer's name?",
-    name: "engineerName",
-}, {
-    type: "input",
-    message: "What is the engineer's id?",
-    name: "engineerId",
-}, {
-    type: "input",
-    message: "What is the engineer's email?",
-    name: "engineerEmail",
-}, {
-    type: "input",
-    message: "What is the engineer's github?",
-    name: "github",
-}]
-const internQuestions = [{
-    type: "input",
-    message: "What is the intern's name?",
-    name: "internName",
-}, {
-    type: "input",
-    message: "What is the intern's id?",
-    name: "internId",
-}, {
-    type: "input",
-    message: "What is the intern's email?",
-    name: "internEmail",
-}, {
-    type: "input",
-    message: "What school does the intern attend?",
-    name: "school",
-}]
+
+const employeeQuestions = [{
+        type: 'list',
+        name: 'role',
+        message: "Please choose your employee's role",
+        choices: ['Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the name of the employee?",
+
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the employee's ID",
+
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the employee's email address?",
+
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's github?",
+        when: (input) => input.role === "Engineer",
+
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "What school does then intern attend?",
+        when: (input) => input.role === "Intern",
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddEmployee',
+        message: 'Would you like to add another team member?',
+        default: false
+    }
+]
+
+
+const addManager = () => {
+    return inquirer.prompt(managerQuestions)
+        .then(managerResponse => {
+            const { name, id, email, officeNumber } = managerResponse;
+            const manager = new Manager(name, id, email, officeNumber);
+
+            teamArr.push(manager);
+            console.log(manager);
+        })
+};
+
+const addEmployee = () => {
+
+    return inquirer.prompt(employeeQuestions)
+        .then(employeeData => {
+            // data for employee types 
+
+            let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+            let employee;
+
+            if (role === "Engineer") {
+                employee = new Engineer(name, id, email, github);
+                console.log(employee);
+            } else if (role === "Intern") {
+                employee = new Intern(name, id, email, school);
+                console.log(employee);
+            }
+
+            teamArr.push(employee);
+
+            if (confirmAddEmployee) {
+                return addEmployee(teamArr);
+            } else {
+                return teamArr;
+            }
+        })
+
+};
+
+
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+            // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+};
+
+addManager()
+    .then(addEmployee)
+    .then(teamArr => {
+        return createHTML(teamArr);
+    })
+    .catch(err => {
+        console.log(err);
+    });
